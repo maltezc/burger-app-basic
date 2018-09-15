8a82ca5d45b01bb9a9814d25b866b11f6d320fbe
cflux1990 <cflux.maltez@live.com>
2018-09-15 07:32:26 -0700

    10.169 - Sending a POST Request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/UI/Modal/Modal'
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
+import axios from '../../axios-orders'
 
 
 //all caps variables are global variables
@@ -89,7 +90,26 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue!');
+        // alert('You continue!');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice, //
+            // for production, do this calculation
+            // on the server so user can manipulate
+            customer: {
+                name: 'Chris Maltez',
+                address: {
+                    street: '745 pine st.',
+                    zipCode: '94108',
+                    country: 'USA'
+                },
+                email: 'test@test.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     }
 
     render() {
