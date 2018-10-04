3001a69b4ccb11aa503c34580308856fe16dd434
cflux1990 <cflux.maltez@live.com>
2018-10-04 07:02:27 -0700

    17.294 - Connecting Contact Data Container & Actions

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -6,6 +6,10 @@ import Spinner from '../../../components/UI/Spinner/Spinner'
 import classes from './ContactData.css'
 import axios from '../../../axios-orders'
 import Input from '../../../components/UI/Input/Input'
+import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler'
+import * as actions from '../../../store/actions/index'
+
+
 
 class ContactData extends Component {
     state = {
@@ -98,7 +102,6 @@ class ContactData extends Component {
 
     orderHandler = ( event ) => {
         event.preventDefault();
-        this.setState({loading: true});
         const formData ={};
         for (let formElementIdentifier in this.state.orderForm) {
             formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
@@ -107,13 +110,10 @@ class ContactData extends Component {
         const order = {
             ingredients: this.props.ings,
             price: this.props.price, //
-            // for production, do this calculation
-            // on the server so user cant manipulate
             orderData: formData,
-
         }
-
-         }
+        this.props.onOrderBurger(order);
+        }
 
 
          checkValidity(value, rules) {
@@ -200,4 +200,8 @@ const mapStateToProps = state => {
     }
 }
 
-export default connect(mapStateToProps)(ContactData)
+const mapDispatchToProps = dispatch => {
+    onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData))
+}
+
+export default connect(mapStateToProps)(withErrorHandler(ContactData, axios))
