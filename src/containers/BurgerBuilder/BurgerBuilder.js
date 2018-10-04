c328687f9d500280837838e744dbb5f063983c79
cflux1990 <cflux.maltez@live.com>
2018-10-04 06:17:29 -0700

    17.290 - Fetching Ingredients Asynchronously

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -8,8 +8,8 @@ import Modal from '../../components/UI/Modal/Modal'
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
 import Spinner from '../../components/UI/Spinner/Spinner'
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
-import axios from '../../axios-orders'
 import * as burgerBuilderActions from '../../store/actions/index'
+import axios from "../../axios-orders";
 
 
 
@@ -23,19 +23,11 @@ class BurgerBuilder extends Component {
 
     state = {
         purchasing: false,
-        loading: false,
-        error: false,
     }
 
     componentDidMount() {
         console.log(this.props);
-        // axios.get('https://react-my-burger-25ef9.firebaseio.com/ingredients.json')
-        //     .then(response => {
-        //         this.setState({ingredients: response.data});
-        //     })
-        //     .catch(error => {
-        //         this.setState({error: true})
-        //     })
+
     }
 
     updatePurchaseState (ingredients) {
@@ -91,9 +83,6 @@ class BurgerBuilder extends Component {
                 price={this.props.price}
                 purchaseCancelled={this.purchaseCancelHandler}
                 purchaseContinued={this.purchaseContinueHandler}/>
-        }
-        if (this.state.loading) {
-            orderSummary = <Spinner/>
         }
 
         return (
