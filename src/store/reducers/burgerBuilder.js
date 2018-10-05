2f57ef78a4505ddd51fd2e8c7dc84d3bebc2e6b7
cflux1990 <cflux.maltez@live.com>
2018-10-05 08:15:55 -0700

    17.301 - Resetting the Price After Purchases

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -46,6 +46,7 @@ const reducer = (state = initialState, action) => {
                     meat: action.ingredients.meat,
 
             },
+                totalPrice: 4,
                 error: false,
             }
         case actionTypes.FETCH_INGREDIENTS_FAILED:
