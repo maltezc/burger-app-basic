46c87f71158b53e670e3f0cf4418627670edac8e
cflux1990 <cflux.maltez@live.com>
2018-10-11 07:24:36 -0700

    18.326 - Guarding Routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom'; // helps with routing
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom'; // helps with routing
 import { connect} from 'react-redux';
 
 import Layout from './hoc/Layout/Layout'
@@ -15,21 +15,40 @@ class App extends Component {
         this.props.onTryAutoSignup();
     }
   render() {
-    return (
-      <div>
-        <Layout>
+      let routes = (
+        <Switch>
+            <Route path='/auth' component={Auth} />
+            <Route path='/' exact component={BurgerBuilder} />
+            <Redirect to='/'/>
+        </Switch>
+    )
+      if (this.props.isAuthenticated) {
+        routes = (
             <Switch>
                 <Route path='/checkout' component={Checkout} />
                 <Route path='/orders' component={Orders} />
-                <Route path='/auth' component={Auth} />
                 <Route path='/logout' component={Logout} />
                 <Route path='/' exact component={BurgerBuilder} />
+                <Redirect to='/'/>
             </Switch>
+        )
+      }
+
+    return (
+      <div>
+        <Layout>
+            {routes}
         </Layout>
 
       </div>
     );
   }
+}
+
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.auth.token !== null
+    }
 }
 
 
@@ -39,4 +58,4 @@ const mapDispatchtoProps = dispatch => {
     }
 }
 
-export default withRouter(connect(null, mapDispatchtoProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchtoProps)(App));
