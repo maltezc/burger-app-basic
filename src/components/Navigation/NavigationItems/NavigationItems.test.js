838577e02fd3a639efe2833dbffb8d5da4a3995b
cflux1990 <cflux.maltez@live.com>
2018-10-12 20:38:21 -0700

    20.345 - Jest and Enzume Documentations

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -12,8 +12,25 @@ import NavigationItem from './NavigationItem/NavigationItem';
 configure({adapter: new Adapter()})
 
 describe('<NavigationItems />', () => {
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallow(<Navigationitems/>);
+    });
+
     it('should render two <NavigationItem /> elements if not authenticated', () => {
-        const wrapper = shallow(<Navigationitems/>);
         expect(wrapper.find(NavigationItem)).toHaveLength(2);
     });
+
+    it('should render three <NavigationItem /> elements if not authenticated', () => {
+        wrapper.setProps({isAuthenticated: true})
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
+    });
+
+    it('should render three <NavigationItem /> elements if not authenticated', () => {
+        wrapper.setProps({isAuthenticated: true})
+        expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>).toEqual(true));
+    });
 })
+
+// each test acts independent of the other
+// check documentation for jest, shallow, expect, enzyme
