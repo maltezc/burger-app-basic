6448410ed5231dfbcbe8154c495ddb095f16bb77
cflux1990 <cflux.maltez@live.com>
2018-10-16 21:37:27 -0700

    25.407 - Moving Logic from the Action Creator to a Saga

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -25,11 +25,11 @@ export const authFail = (error) => {
 };
 
 export const logout = () => {
-    localStorage.removeItem('token');
-    localStorage.removeItem('expirationDate');
-    localStorage.removeItem('userId');
+    // localStorage.removeItem('token');
+    // localStorage.removeItem('expirationDate');
+    // localStorage.removeItem('userId');
     return {
-        type: actionTypes.AUTH_LOGOUT
+        type: actionTypes.AUTH_INITIATE_LOGOUT
     }
 }
 
@@ -37,14 +37,14 @@ export const logout = () => {
 export const checkAuthTimeout = (expirationTime) => {
     return dispatch => {
         setTimeout(() => {
-            // dispatch(logout());
+            dispatch(logout());
         }, expirationTime * 1000);
     }
 }
 
 export const auth = (email, password, isSignup) => {
     return dispatch => {
-        // dispatch(authStart());
+        dispatch(authStart());
         const authData = {
             email: email,
             password: password,
@@ -82,14 +82,14 @@ export const authCheckState = () => {
     return dispatch => {
         const token = localStorage.getItem('token');
         if (!token) {
-            // dispatch(logout());
+            dispatch(logout());
         } else {
             const expirationDate = new Date(localStorage.getItem('expirationDate'));
             if (expirationDate <= new Date()) {
                 // dispatch(logout());
             } else {
                 const userId = localStorage.getItem('userId');
-                dispatch(authSuccess(token, userId))
+                // dispatch(authSuccess(token, userId))
                 dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime()) / 1000 )); // gives time in miliseconds
             }
         }
